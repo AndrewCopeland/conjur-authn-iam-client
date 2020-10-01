d982cb88887886efd9b631576b33dbad286f4cb6
AndrewCopeland <andcope1995@gmail.com>
2020-10-01 12:09:01 -0400

    Still using conjur-api-go but will only work with CA signed cert

--- pkg/authenticator/conjur/retrieve_secret.go
+++ pkg/authenticator/conjur/retrieve_secret.go
@@ -6,12 +6,18 @@ import (
 	"github.com/cyberark/conjur-api-go/conjurapi"
 )
 
+// RetrieveSecret from conjur, if secretID is "" then no error will be returned but value will be (nil. nil)
 func RetrieveSecret(config Config, accessToken string, secretID string) ([]byte, error) {
 	if secretID == "" {
 		return nil, nil
 	}
 
-	client, err := conjurapi.NewClientFromToken(config.Config, string(accessToken))
+	conjurConfig := conjurapi.Config{
+		Account:      config.Account,
+		ApplianceURL: config.ApplianceURL,
+	}
+
+	client, err := conjurapi.NewClientFromToken(conjurConfig, string(accessToken))
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create the Conjur client. %s", err)
 	}
